49d885d1929963123dccf7b5ae32565185090cb2
DESKTOP-J9Q009D\undefined <team-neptune@outlook.com>
2025-04-02 01:14:18 +0330

    some error handling

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,37 @@
+use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
     println!("Guess the Number!");
 
-    println!("Please Input Your Guess: ");
+    let secret_number = rand::thread_rng().gen_range(1..=100);
 
-    let mut guess = String::new();
+    loop {
+        println!("Please Input Your Guess: ");
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed To Read Line...");
-    println!("You Guessed : {}", guess);
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed To Read Line...");
+
+        // let guess: u32 = guess.trim().parse().expect("Please type a Number!");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You Guessed : {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too Small!"),
+            Ordering::Greater => println!("Too Big!"),
+            Ordering::Equal => {
+                println!("You Win...");
+                break;
+            }
+        }
+    }
 }
